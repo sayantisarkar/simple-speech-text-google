90854680a5953f0e2a93b4020c03ae6bb42eddfa
sayantisarkar <sayantisarkar088@gmail.com>
2021-03-15 16:42:53 +0400

    Fixed the blob part of the audio file

--- src/SpeechTranscribeService.js
+++ src/SpeechTranscribeService.js
@@ -15,13 +15,9 @@ class SpeechTranscribeService {
 
 
         var audioFile = new File([searchRecord.blob], "myaudio.webm", {type: searchRecord.options.mimeType});
-        //var audioFile = searchRecord.blob;
-        //const audioFile = new Blob(searchRecord, { 'type': 'audio/webm; codecs=opus' });
         const formData = new FormData();
         formData.append('sourceFile', audioFile);
-        formData.append('duration',searchRecord.stopTime-searchRecord.startTime);
-        //console.log("Check file:"+audioFile.name);
-
+        
         // return axios.post(SEARCH_REST_API_URL,formData);
 
 
