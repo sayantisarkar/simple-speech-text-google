bad568aba6d1ac6e2913e2547f05d2c7363cb2cf
sayantisarkar <sayantisarkar088@gmail.com>
2021-03-17 16:34:57 +0400

    Changes to show in UI- not completely working though

--- src/SpeechTranscribeService.js
+++ src/SpeechTranscribeService.js
@@ -1,37 +1,56 @@
 import axios from 'axios'
+import { Component } from 'react'
+
 
 const TRANSCRIBE_REST_API_URL = 'http://localhost:8080/speech/transcribe';
 const SEARCH_REST_API_URL = 'http://localhost:8080/speech/search';
 
 
-class SpeechTranscribeService {
+class SpeechTranscribeService extends Component{
 
 
+  constructor(){
+     super();
+     this.result = '';
+  }
+
+    // componentDidMount(searchRecord){
+    //   this.getSearchTranscription(searchRecord);
+    // }
 
     getTranscription(){
         return axios.get(TRANSCRIBE_REST_API_URL);
     }
     getSearchTranscription(searchRecord){
-
-
-        var audioFile = new File([searchRecord.blob], "myaudio.webm", {type: searchRecord.options.mimeType});
-        const formData = new FormData();
-        formData.append('sourceFile', audioFile);
-        
         // return axios.post(SEARCH_REST_API_URL,formData);
 
+          var audioFile = new File([searchRecord.blob], "myaudio.webm", {type: searchRecord.options.mimeType});
+          const formData = new FormData();
+          formData.append('sourceFile', audioFile);
+          axios({
+            method : 'post',
+            url : SEARCH_REST_API_URL,
+            data : formData,
+            headers : {
+                  'Content-Type': 'multipart/form-data'
+            }
+          }).then((res)=>{
+            console.log(res);
+            this.result = res.data;
+            // this.setState({
+            //   transcripts: res.data
+            // });
+          }).catch((err) => {
+            throw err});
+    }
 
-        axios({
-          method : 'post',
-          url : SEARCH_REST_API_URL,
-          data : formData,
-          headers : {
-                'Content-Type': 'multipart/form-data'
-          }
-        }).then((res)=>{
-          console.log("Your search transcript is",res);
-        }).catch((err) => {
-          throw err});
-     }
+    render() {
+        return (
+          <ul>
+            <li>{this.result}</li>
+          </ul>
+        )
+      }
 }
+
 export default new SpeechTranscribeService();
